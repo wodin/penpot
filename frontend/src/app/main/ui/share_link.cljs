3d7a433c5cda20615329ada0e4a55d30f9326370
Eva <eva.marco@kaleidos.net>
2022-06-16 12:56:22 +0200

    wip

--- frontend/src/app/main/ui/share_link.cljs
+++ frontend/src/app/main/ui/share_link.cljs
@@ -34,17 +34,19 @@
   {::mf/register modal/components
    ::mf/register-as :share-link}
   [{:keys [file page]}]
-  (let [slinks  (mf/deref refs/share-links)
-        router  (mf/deref refs/router)
-        route   (mf/deref refs/route)
+  (let [slinks   (mf/deref refs/share-links)
+        router   (mf/deref refs/router)
+        route    (mf/deref refs/route)
 
-        link    (mf/use-state nil)
-        confirm (mf/use-state false)
+        link     (mf/use-state nil)
+        confirm  (mf/use-state false)
+        open-ops (mf/use-state false)
 
-        opts    (mf/use-state
-                 {:sections #{"viewer"}
-                  :pages-mode "current"
-                  :pages #{(:id page)}})
+        opts     (mf/use-state
+                  {:pages-mode "current"
+                   :pages #{(:id page)}
+                   :who-comment "team"
+                   :who-inspect "team"})
 
         close
         (fn [event]
@@ -98,6 +100,18 @@
                       (dm/show {:type :info
                                 :content (tr "common.share-link.link-deleted-success")
                                 :timeout 3000}))))
+        
+        manage-open-ops
+        (fn [_]
+          (swap! open-ops not))
+        
+        on-who-change
+        (fn [type value]
+          (if (= type :comment)
+            (swap! opts assoc :who-comment value)
+            (swap! opts assoc :who-inspect value)
+            )
+          )
         ]
 
     (mf/use-effect
@@ -114,121 +128,126 @@
                         (assoc cf/public-uri :fragment href)))]
          (reset! link (some-> href str)))))
 
-    [:div.modal-overlay
+    [:div.modal-overlay.share-modal
      [:div.modal-container.share-link-dialog
-      [:div.modal-content
+      [:div.modal-content.initial
        [:div.title
         [:h2 (tr "common.share-link.title")]
         [:div.modal-close-button
          {:on-click close
           :title (tr "labels.close")}
-         i/close]]
-
-       [:div.share-link-section
-        [:label (tr "labels.link")]
-        [:div.custom-input.with-icon
-         [:input {:type "text"
-                  :value (or @link "")
-                  :placeholder (tr "common.share-link.placeholder")
-                  :read-only true}]
-         (when (some? @link)
-           [:div.help-icon {:title (tr "labels.copy")
-                            :on-click copy-link}
-            i/copy])]
-
-        [:div.hint (tr "common.share-link.permissions-hint")]]]
-
+         i/close]]]
       [:div.modal-content
-       (let [sections (:sections @opts)]
-         [:div.access-mode
-          [:div.title (tr "common.share-link.permissions-can-access")]
-          [:div.items
-           [:div.input-checkbox.check-primary.disabled
-            [:input.check-primary.input-checkbox {:type "checkbox" :disabled true}]
-            [:label (tr "labels.workspace")]]
+       [:div.share-link-section
+        (when (and (not @confirm) (some? @link))
+          [:div.custom-input.with-icon
+           [:input {:type "text"
+                    :value (or @link "")
+                    :placeholder (tr "common.share-link.placeholder")
+                    :read-only true}]
+           [:div.help-icon {:title (tr "viewer.header.share.copy-link")
+                            :on-click copy-link}
+            i/copy]])
+        [:div.hint-wrapper
+         (when (not @confirm)[:div.hint (tr "common.share-link.permissions-hint")])
+         (cond
+           (true? @confirm)
+           [:div.confirm-dialog
+            [:div.description (tr "common.share-link.confirm-deletion-link-description")]
+            [:div.actions
+             [:input.btn-secondary
+              {:type "button"
+               :on-click #(reset! confirm false)
+               :value (tr "labels.cancel")}]
+             [:input.btn-warning
+              {:type "button"
+               :on-click delete-link
+               :value (tr "common.share-link.remove-link")}]]]
 
-           [:div.input-checkbox.check-primary
-            [:input {:type "checkbox"
-                     :default-checked (contains? sections "viewer")}]
-            [:label (tr "labels.viewer")
-             [:span.hint "(" (tr "labels.default") ")"]]]
-
-           ;; [:div.input-checkbox.check-primary
-           ;;  [:input.check-primary.input-checkbox {:type "checkbox"}]
-           ;;  [:label "Handoff" ]]
-           ]])
-
-       (let [mode (:pages-mode @opts)]
-         [:*
-          [:div.view-mode
-           [:div.title (tr "common.share-link.permissions-can-view")]
-           [:div.items
-            [:div.input-radio.radio-primary
-             [:input {:type "radio"
-                      :id "view-all"
-                      :checked (= "all" mode)
-                      :name "pages-mode"
-                      :on-change #(select-pages-mode "all")}]
-             [:label {:for "view-all"} (tr "common.share-link.view-all-pages")]]
-
-            [:div.input-radio.radio-primary
-             [:input {:type "radio"
-                      :id "view-current"
-                      :name "pages-mode"
-                      :checked (= "current" mode)
-                      :on-change #(select-pages-mode "current")}]
-             [:label {:for "view-current"} (tr "common.share-link.view-current-page")]]
-
-            [:div.input-radio.radio-primary
-             [:input {:type "radio"
-                      :id "view-selected"
-                      :name "pages-mode"
-                      :checked (= "selected" mode)
-                      :on-change #(select-pages-mode "selected")}]
-             [:label {:for "view-selected"} (tr "common.share-link.view-selected-pages")]]]]
-
-          (when (= "selected" mode)
-            (let [pages   (->> (get-in file [:data :pages])
-                               (map #(get-in file [:data :pages-index %])))
-                  selected (:pages @opts)]
-              [:ul.pages-selection
-               (for [page pages]
-                 [:li.input-checkbox.check-primary {:key (str (:id page))}
-                  [:input {:type "checkbox"
-                           :id (str "page-" (:id page))
-                           :on-change #(mark-checked-page % (:id page))
-                           :checked (contains? selected (:id page))}]
-                  [:label {:for (str "page-" (:id page))} (:name page)]])]))])]
-
-      [:div.modal-footer
-       (cond
-         (true? @confirm)
-         [:div.confirm-dialog
-          [:div.description (tr "common.share-link.confirm-deletion-link-description")]
-          [:div.actions
+           (some? @link)
            [:input.btn-secondary
             {:type "button"
-             :on-click #(reset! confirm false)
-             :value (tr "labels.cancel")}]
-           [:input.btn-warning
+             :class "primary"
+             :on-click try-delete-link
+             :value (tr "common.share-link.remove-link")}]
+
+           :else
+           [:input.btn-primary
             {:type "button"
-             :on-click delete-link
-             :value (tr "common.share-link.remove-link")
-             }]]]
+             :class "primary"
+             :on-click create-link
+             :value (tr "common.share-link.get-link")}])]
+        ]]
+      [:div.modal-content.ops-section
+       [:div.manage-permissions
+        {:on-click manage-open-ops}
+        [:span.icon i/picker-hsv]
+        [:div.title (tr "common.share-link.manage-ops")]]
+       (when @open-ops
+         [:*
+          (let [mode (:pages-mode @opts)]
+            [:*
+             [:div.view-mode
+              [:div.subtitle
+               [:span.icon i/play]
+               (tr "common.share-link.permissions-pages")]
+              [:div.items
+               [:div.input-radio.radio-primary
+                [:input {:type "radio"
+                         :id "view-all"
+                         :checked (= "all" mode)
+                         :name "pages-mode"
+                         :on-change #(select-pages-mode "all")}]
+                [:label {:for "view-all"} (tr "common.share-link.view-all")]]
 
-         (some? @link)
-         [:input.btn-secondary
-          {:type "button"
-           :class "primary"
-           :on-click try-delete-link
-           :value (tr "common.share-link.remove-link")}]
+               [:div.input-radio.radio-primary
+                [:input {:type "radio"
+                         :id "view-current"
+                         :name "pages-mode"
+                         :checked (= "current" mode)
+                         :on-change #(select-pages-mode "current")}]
+                [:label {:for "view-current"} (tr "common.share-link.view-current")]]
 
-         :else
-         [:input.btn-primary
-          {:type "button"
-           :class "primary"
-           :on-click create-link
-           :value (tr "common.share-link.get-link")}])]
+               [:div.input-radio.radio-primary
+                [:input {:type "radio"
+                         :id "view-selected"
+                         :name "pages-mode"
+                         :checked (= "selected" mode)
+                         :on-change #(select-pages-mode "selected")}]
+                [:label {:for "view-selected"} (tr "common.share-link.view-selected")]]]]
+
+             (when (= "selected" mode)
+               (let [pages   (->> (get-in file [:data :pages])
+                                  (map #(get-in file [:data :pages-index %])))
+                     selected (:pages @opts)]
+                 [:ul.pages-selection
+                  (for [page pages]
+                    [:li.input-checkbox.check-primary {:key (str (:id page))}
+                     [:input {:type "checkbox"
+                              :id (str "page-" (:id page))
+                              :on-change #(mark-checked-page % (:id page))
+                              :checked (contains? selected (:id page))}]
+                     [:label {:for (str "page-" (:id page))} (:name page)]])]))])
+          [:div.access-mode
+           [:div.subtitle
+            [:span.icon i/chat]
+            (tr "common.share-link.permissions-can-comment")]
+           [:div.items
+            [:select.input-select {:on-change (partial on-who-change :comment)
+                                   :value (:who-comment @opts)}
+             [:option {:value "team"}  (tr "common.share-link.team-members")]
+             [:option {:value "all"}  (tr "common.share-link.all-users")]]]]
+          [:div.inspect-mode
+           [:div.subtitle
+            [:span.icon i/code]
+            (tr "common.share-link.permissions-can-inspect")]
+           [:div.items
+            [:select.input-select {:on-change (partial on-who-change :inspect)
+                                   :value (:who-inspect @opts)}
+             [:option {:value "team"}  (tr "common.share-link.team-members")]
+             [:option {:value "all"}  (tr "common.share-link.all-users")]]]]]
+
+         )]
 
       ]]))
 
